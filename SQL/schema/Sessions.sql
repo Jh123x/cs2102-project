8f8a4677899d0f479b8d0efa697a5c32917ed014
Junhua <wenjh1998@gmail.com>
2021-02-25 16:46:39 +0800

    Create Sessions.sql

---
+++ SQL/schema/Sessions.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS Sessions(
+    sid INTEGER PRIMARY KEY,
+    date DATE NOT NULL,
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP NOT NULL,
+    course_id INTEGER REFERENCES Offerings,
+    unique(course_id, date)
+)
