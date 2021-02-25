3d840914ee65a5446bf56561ce7aa282d7002d23
Junhua <wenjh1998@gmail.com>
2021-02-25 16:39:45 +0800

    Update Rooms.sql

--- SQL/schema/Rooms.sql
+++ SQL/schema/Rooms.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS ROOMS (
     rid Integer Primary key,
-    location Text,
+    location Text not NULL,
     seating_capacity Integer,
     check(seating_capacity > 0),
     unique(location)
