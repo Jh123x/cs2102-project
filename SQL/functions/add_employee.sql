1a55b6b5333b0c98074eabf5d5fccdc86e6e93cc
Wen Junhua <wenjh1998@gmail.com>
2021-03-21 19:23:04 +0800

    Update add_employee.sql
    Generate eid automatically
    Add course area if found

--- SQL/functions/add_employee.sql
+++ SQL/functions/add_employee.sql
@@ -1,5 +1,4 @@
 CREATE OR REPLACE PROCEDURE add_employee (
-        eid INTEGER PRIMARY KEY,
         name TEXT,
         address TEXT,
         phone TEXT,
@@ -7,9 +6,14 @@ CREATE OR REPLACE PROCEDURE add_employee (
         join_date date,
         category TEXT, -- Manager / Admin / Instructor
         type TEXT, -- Full Time / Part Time
-        salary_amount numeric -- hourly_rate for part-time and monthly_salary for full-time
+        salary_amount numeric, -- hourly_rate for part-time and monthly_salary for full-time
+        course_area TEXT[]
     ) AS $$
+DECLARE 
+    eid INTEGER;
 BEGIN
+    -- Generate the next eid
+    SELECT MAX(e.eid) + 1 INTO eid FROM Employees;
     INSERT INTO Employees
     VALUES (
             eid,
@@ -33,14 +37,32 @@ BEGIN
     -- Add into role specific table
     IF (category = "Manager") THEN
         INSERT INTO Managers VALUES (eid);
+
+        -- Add them to the specified course area
+        FOR area IN course_area 
+        LOOP
+            INSERT INTO Course_areas VALUES (area, eid);
+        END LOOP;
     ELSE IF (category = "Admin") THEN
         INSERT INTO Administrators VALUES (eid);
-    ELSE IF (category = "Part-time-Instructor") THEN
-        INSERT INTO PartTimeInstructors VALUES (eid);
-    ELSE IF (category = "Full-time-Instructor") THEN
-        INSERT INTO FullTimeInstructors VALUES (eid);
+        IF (course_area.COUNT > 0) THEN
+            RAISE EXCEPTION "Admin should not have course area";
+    ELSE IF (category = "Instructor") THEN
+        INSERT INTO Instructors VALUES (eid);
+        FOR area IN course_area 
+        LOOP
+            INSERT INTO Specializes VALUES (eid, area);
+        END LOOP;
+        IF (type = "part-time") THEN
+            INSERT INTO PartTimeInstructors VALUES (eid);
+        ELSE IF (type = 'full-time') THEN
+            INSERT INTO FullTimeInstructors VALUES (eid);
+        ENDIF;
     ELSE
         RAISE EXCEPTION "Category not found";
     ENDIF;
+
+    
+
 END
 $$ LANGUAGE plpgsql;
