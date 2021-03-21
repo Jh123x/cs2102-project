c45dc0c2aae2c656ac3c1d4a3b4cb5286c39b798
Wen Junhua <wenjh1998@gmail.com>
2021-03-21 18:04:54 +0800

    Fix Missing Begin and End

--- SQL/functions/add_employee.sql
+++ SQL/functions/add_employee.sql
@@ -9,36 +9,38 @@ CREATE OR REPLACE PROCEDURE add_employee (
         type TEXT, -- Full Time / Part Time
         salary_amount numeric -- hourly_rate for part-time and monthly_salary for full-time
     ) AS $$
-INSERT INTO Employees
-VALUES (
-        eid,
-        name,
-        address,
-        phone,
-        email,
-        join_date,
-        NULL -- No depart date yet
-    );
+BEGIN
+    INSERT INTO Employees
+    VALUES (
+            eid,
+            name,
+            address,
+            phone,
+            email,
+            join_date,
+            NULL -- No depart date yet
+        );
 
--- Add into part-time / full time
-IF (type = 'part-time') THEN
-    INSERT INTO PartTimeEmployees VALUES (eid, salary_amount);
-ELSE IF (type = 'full-time') THEN
-    INSERT INTO FullTimeEmployees VALUES (eid, salary_amount);
-ELSE
-    RAISE EXCEPTION "Type not found";
-ENDIF;
+    -- Add into part-time / full time
+    IF (type = 'part-time') THEN
+        INSERT INTO PartTimeEmployees VALUES (eid, salary_amount);
+    ELSE IF (type = 'full-time') THEN
+        INSERT INTO FullTimeEmployees VALUES (eid, salary_amount);
+    ELSE
+        RAISE EXCEPTION "Type not found";
+    ENDIF;
 
--- Add into role specific table
-IF (category = "Manager") THEN
-    INSERT INTO Managers VALUES (eid);
-ELSE IF (category = "Admin") THEN
-    INSERT INTO Administrators VALUES (eid);
-ELSE IF (category = "Part-time-Instructor") THEN
-    INSERT INTO PartTimeInstructors VALUES (eid);
-ELSE IF (category = "Full-time-Instructor") THEN
-    INSERT INTO FullTimeInstructors VALUES (eid);
-ELSE
-    RAISE EXCEPTION "Category not found";
-ENDIF;
+    -- Add into role specific table
+    IF (category = "Manager") THEN
+        INSERT INTO Managers VALUES (eid);
+    ELSE IF (category = "Admin") THEN
+        INSERT INTO Administrators VALUES (eid);
+    ELSE IF (category = "Part-time-Instructor") THEN
+        INSERT INTO PartTimeInstructors VALUES (eid);
+    ELSE IF (category = "Full-time-Instructor") THEN
+        INSERT INTO FullTimeInstructors VALUES (eid);
+    ELSE
+        RAISE EXCEPTION "Category not found";
+    ENDIF;
+END
 $$ LANGUAGE plpgsql;
