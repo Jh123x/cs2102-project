2692e21854b7fa546a8ee31bad670b43dc3f984f
Wen Junhua <wenjh1998@gmail.com>
2021-03-21 18:02:51 +0800

    Put Credit Card together with customer

--- SQL/schema/Credit_Cards.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS Credit_cards CASCADE;
-CREATE TABLE Credit_cards (
-    number CHAR(16) PRIMARY KEY,
-    cvv CHAR(3),
-    expiry_date DATE,
-    from_date DATE
-);
--- SQL/schema/Customers.sql
+++ SQL/schema/Customers.sql
@@ -6,3 +6,11 @@ CREATE TABLE Customers (
     name TEXT,
     email TEXT
 );
+
+DROP TABLE IF EXISTS Credit_cards CASCADE;
+CREATE TABLE Credit_cards (
+    number CHAR(16) PRIMARY KEY,
+    cvv CHAR(3),
+    expiry_date DATE,
+    from_date DATE
+);
