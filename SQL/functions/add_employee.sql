dcbb98f720778896e5fe793215e7bb77e2878949
Wen Junhua <wenjh1998@gmail.com>
2021-03-21 18:00:03 +0800

    Update add_employee.sql
    Add more checks and include adding into the different types

---
+++ SQL/functions/add_employee.sql
@@ -0,0 +1,44 @@
+CREATE OR REPLACE PROCEDURE add_employee (
+        eid INTEGER PRIMARY KEY,
+        name TEXT,
+        address TEXT,
+        phone TEXT,
+        email TEXT,
+        join_date date,
+        category TEXT, -- Manager / Admin / Instructor
+        type TEXT, -- Full Time / Part Time
+        salary_amount numeric -- hourly_rate for part-time and monthly_salary for full-time
+    ) AS $$
+INSERT INTO Employees
+VALUES (
+        eid,
+        name,
+        address,
+        phone,
+        email,
+        join_date,
+        NULL -- No depart date yet
+    );
+
+-- Add into part-time / full time
+IF (type = 'part-time') THEN
+    INSERT INTO PartTimeEmployees VALUES (eid, salary_amount);
+ELSE IF (type = 'full-time') THEN
+    INSERT INTO FullTimeEmployees VALUES (eid, salary_amount);
+ELSE
+    RAISE EXCEPTION "Type not found";
+ENDIF;
+
+-- Add into role specific table
+IF (category = "Manager") THEN
+    INSERT INTO Managers VALUES (eid);
+ELSE IF (category = "Admin") THEN
+    INSERT INTO Administrators VALUES (eid);
+ELSE IF (category = "Part-time-Instructor") THEN
+    INSERT INTO PartTimeInstructors VALUES (eid);
+ELSE IF (category = "Full-time-Instructor") THEN
+    INSERT INTO FullTimeInstructors VALUES (eid);
+ELSE
+    RAISE EXCEPTION "Category not found";
+ENDIF;
+$$ LANGUAGE plpgsql;
