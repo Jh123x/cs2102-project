037551eabfc67965d2111977ba7780ecdc195945
Wen Junhua <wenjh1998@gmail.com>
2021-03-21 18:31:54 +0800

    Update remove_employee.sql

---
+++ SQL/functions/remove_employee.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE PROCEDURE remove_employee(
+    eid INTEGER,
+    departure_date DATE
+) AS $$
+
+DECLARE
+    isAdmin_count INTEGER;
+    isManaging_count INTEGER;
+    isTeaching_count INTEGER;
+
+BEGIN
+    -- Check if they are still handling admin tasks
+    SELECT COUNT(*) INTO isAdmin_count FROM CourseOfferings c WHERE c.admin_id = eid;
+
+    -- Check if they are still teaching some course
+    SELECT COUNT(*) INTO isManaging_count FROM Course_areas c WHERE c.manager_id = eid;
+
+    -- Check if they are manager managing some area
+    SELECT COUNT(*) INTO isTeaching_count FROM Sessions WHERE s.instructor_id = eid;
+
+    IF (isAdmin_count + isManaging_count + isTeaching_count > 0) THEN 
+        RAISE EXCEPTION "Employing is still Admin/Managing/Teaching"
+    ENDIF
+
+    DELETE FROM Employees e WHERE e.eid = eid;
+
+END
+$$ LANGUAGE plpgsql
