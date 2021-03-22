156fdb1b66fbc0f68c49e10212b06951ff30e28e
Wen Junhua <wenjh1998@gmail.com>
2021-03-22 12:11:55 +0800

    Update session_collision_check.sql

--- SQL/triggers/session_collision_check.sql
+++ SQL/triggers/session_collision_check.sql
@@ -1,1 +1,39 @@
 -- Make sure no 2 sessions are using the same room at the same time.
+CREATE OR REPLACE FUNCTION session_collision_check() RETURNS TRIGGER AS $$
+DECLARE
+    collisions INTEGER;
+begin
+
+    -- Check if the old time and date = new time and date    
+    IF (TG_OP = 'UPDATE' AND NEW.date = OLD.date AND NEW.start_time = OLD.start_time
+        RETURN NEW;
+
+    -- Check if it is a valid operation
+    ELSE IF (TG_OP NOT IN ('INSERT', 'UPDATE')) THEN
+        RAISE EXCEPTION "Trigger is not suppose to be enforces in other methods.";
+    END IF;
+
+    -- Find collisions
+    SELECT COUNT(*) INTO collisions_same_course_offering 
+    FROM Sessions s
+    WHERE NEW.date = s.date
+    AND (
+        NEW.start_time BETWEEN s.start_time and s.end_time
+        OR
+        s.start_time BETWEEN NEW.start_time AND NEW.end_time
+    )
+    AND (NEW.course_id = s.course_id OR NEW.rid = s.rid);
+  
+    -- Filter if there are any collisions
+    IF (collisions > 0) THEN
+        RAISE NOTICE "There is a collision with the current session."
+        RETURN NULL;
+
+    -- If everything passes return NEW
+    RETURN NEW;
+end;
+$$ LANGUAGE plpgsql
+
+CREATE TRIGGER session_collision_trigger
+BEFORE INSERT OR UPDATE ON Sessions
+FOR EACH ROW EXECUTE FUNCTION session_collision_check();
