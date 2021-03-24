fcc79b280ab656d2a1a851f9310aa72bb9b2996c
Wen Junhua <wenjh1998@gmail.com>
2021-03-24 16:57:13 +0800

    Update registration_deadline_check.sql

--- SQL/triggers/registration_deadline_check.sql
+++ SQL/triggers/registration_deadline_check.sql
@@ -1,1 +1,12 @@
 -- Registration deadline 10 days before lesson start
+CREATE OR REPLACE FUNCTION registration_deadline_check() RETURNS TRIGGER AS $$
+    DECLARE
+
+    BEGIN
+        
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER registration_deadline_trigger
+BEFORE INSERT OR UPDATE ON Registers
+FOR EACH ROW EXECUTE FUNCTION registration_deadline_check();
