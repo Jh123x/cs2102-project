d4f2dd3f994f0f627f8ed4acf5e194f6efdb4b1d
Junhua <wenjh1998@gmail.com>
2021-03-27 23:33:57 +0800

    Make the trigger shorter

--- SQL/triggers/part_full_time_check.sql
+++ SQL/triggers/part_full_time_check.sql
@@ -1,11 +1,6 @@
 CREATE OR REPLACE FUNCTION part_full_time_check() RETURNS TRIGGER AS $$
-    DECLARE
-        full_time_count INTEGER;
-        part_time_count INTEGER;
     BEGIN
-        SELECT COUNT(*) INTO full_time_count FROM FullTimeEmployees WHERE eid = NEW.eid;
-        SELECT COUNT(*) INTO part_time_count FROM PartTimeEmployees WHERE eid = NEW.eid;
-        IF (full_time_count + part_time_count > 0)
+        IF (NEW.eid IN (SELECT eid FROM PartTimeEmployees UNION SELECT eid FROM FullTimeEmployees))
         THEN
             RAISE EXCEPTION 'Employee already exists in part time or full time role';
         END IF;
