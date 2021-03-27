8085c1de80690ea828146a7fb8dbc3df5b5a374f
Junhua <wenjh1998@gmail.com>
2021-03-27 21:20:37 +0800

    Update Rooms.sql

--- SQL/schema/Rooms.sql
+++ SQL/schema/Rooms.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS Rooms CASCADE;
 CREATE TABLE Rooms (
     rid Integer Primary key,
     location Text UNIQUE NOT NULL,
-    seating_capacity Integer,
+    seating_capacity Integer NOT NULL,
     check(seating_capacity > 0),
 );
