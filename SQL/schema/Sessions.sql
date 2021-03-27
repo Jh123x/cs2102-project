61d4a6d8ff98d9456d597dba8c6d73b77ffb0588
Junhua <wenjh1998@gmail.com>
2021-03-27 22:07:15 +0800

    Update Sessions.sql

--- SQL/schema/Sessions.sql
+++ SQL/schema/Sessions.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS Sessions CASCADE;
 CREATE TABLE Sessions (
-    sid INTEGER UNIQUE NOT NULL,
-    /* Session number is suppose to follow course offering */
+    sid INTEGER UNIQUE NOT NULL, /* Session number is suppose to follow course offering */
     date DATE NOT NULL,
     start_time TIMESTAMP NOT NULL,
     end_time TIMESTAMP NOT NULL,
@@ -10,55 +9,16 @@ CREATE TABLE Sessions (
     rid INTEGER REFERENCES Rooms,
     instructor_id INTEGER REFERENCES Instructors,
     unique(course_id, date),
+
     PRIMARY KEY(sid, course_id, launch_date),
     FOREIGN KEY(launch_date, course_id) REFERENCES CourseOfferings(launch_date, course_id) ON DELETE CASCADE,
-    CHECK(end_time > start_time),
+
     -- Check if the session is conducted between 9am - 12pm to 2pm - 6pm and between
-    CHECK(
-        (
-            9 <= EXTRACT(
-                HOUR
-                FROM start_time
-            )
-            AND EXTRACT(
-                HOUR
-                FROM start_time
-            ) <= 12
-        )
-        OR (
-            14 <= EXTRACT(
-                HOUR
-                FROM start_time
-            )
-            AND EXTRACT(
-                HOUR
-                FROM start_time
-            ) <= 18
-        )
-    ),
-    CHECK(
-        (
-            9 <= EXTRACT(
-                HOUR
-                FROM end_time
-            )
-            AND EXTRACT(
-                HOUR
-                FROM end_time
-            ) <= 12
-        )
-        OR (
-            14 <= EXTRACT(
-                HOUR
-                FROM end_time
-            )
-            AND EXTRACT(
-                HOUR
-                FROM end_time
-            ) <= 18
-        )
-    )
+    CHECK((9 <= EXTRACT(HOUR FROM start_time) AND EXTRACT(HOUR FROM start_time) <= 12) OR (14 <= EXTRACT(HOUR FROM start_time) AND EXTRACT(HOUR FROM start_time) <= 18)),
+    CHECK((9 <= EXTRACT(HOUR FROM end_time) AND EXTRACT(HOUR FROM end_time) <= 12) OR (14 <= EXTRACT(HOUR FROM end_time) AND EXTRACT(HOUR FROM end_time) <= 18)),
+    CHECK(end_time > start_time)
 );
+
 DROP TABLE IF EXISTS Cancels CASCADE;
 CREATE TABLE Cancels (
     date TIMESTAMP PRIMARY KEY,
