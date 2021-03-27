6627f5155964531513c4cd8070868fc818d3e6b8
Junhua <wenjh1998@gmail.com>
2021-03-27 22:09:50 +0800

    Update Specializes.sql

--- SQL/schema/Specializes.sql
+++ SQL/schema/Specializes.sql
@@ -1,8 +1,8 @@
 drop table if exists Specializes cascade;
 create table Specializes (
     instructor_id INTEGER,
-    cArea_name TEXT,
+    course_area_name TEXT,
     PRIMARY KEY(instructor_id, course_area_name),
     FOREIGN KEY(instructor_id) REFERENCES Instructors,
-    FOREIGN KEY(course_area_name) REFERENCES Course_areas
+    FOREIGN KEY(course_area_name) REFERENCES CourseAreas
 );
