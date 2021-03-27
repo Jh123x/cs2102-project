81024967aeb803a5e456d2cc65f29770cde80541
Junhua <wenjh1998@gmail.com>
2021-03-27 22:06:30 +0800

    Update Sessions.sql

--- SQL/schema/Sessions.sql
+++ SQL/schema/Sessions.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS Sessions CASCADE;
 CREATE TABLE Sessions (
-    sid INTEGER UNIQUE NOT NULL, /* Session number is suppose to follow course offering */
+    sid INTEGER UNIQUE NOT NULL,
+    /* Session number is suppose to follow course offering */
     date DATE NOT NULL,
     start_time TIMESTAMP NOT NULL,
     end_time TIMESTAMP NOT NULL,
@@ -13,15 +14,57 @@ CREATE TABLE Sessions (
     FOREIGN KEY(launch_date, course_id) REFERENCES CourseOfferings(launch_date, course_id) ON DELETE CASCADE,
     CHECK(end_time > start_time),
     -- Check if the session is conducted between 9am - 12pm to 2pm - 6pm and between
-    CHECK((9 <= EXTRACT(HOUR FROM start_time) AND EXTRACT(HOUR FROM start_time) <= 12) OR (14 <= EXTRACT(HOUR FROM start_time) AND EXTRACT(HOUR FROM start_time) <= 18)),
-    CHECK((9 <= EXTRACT(HOUR FROM end_time) AND EXTRACT(HOUR FROM end_time) <= 12) OR (14 <= EXTRACT(HOUR FROM end_time) AND EXTRACT(HOUR FROM end_time) <= 18))    
+    CHECK(
+        (
+            9 <= EXTRACT(
+                HOUR
+                FROM start_time
+            )
+            AND EXTRACT(
+                HOUR
+                FROM start_time
+            ) <= 12
+        )
+        OR (
+            14 <= EXTRACT(
+                HOUR
+                FROM start_time
+            )
+            AND EXTRACT(
+                HOUR
+                FROM start_time
+            ) <= 18
+        )
+    ),
+    CHECK(
+        (
+            9 <= EXTRACT(
+                HOUR
+                FROM end_time
+            )
+            AND EXTRACT(
+                HOUR
+                FROM end_time
+            ) <= 12
+        )
+        OR (
+            14 <= EXTRACT(
+                HOUR
+                FROM end_time
+            )
+            AND EXTRACT(
+                HOUR
+                FROM end_time
+            ) <= 18
+        )
+    )
 );
 DROP TABLE IF EXISTS Cancels CASCADE;
 CREATE TABLE Cancels (
     date TIMESTAMP PRIMARY KEY,
-	refund_amt MONEY,
-	package_credit INTEGER,
+    refund_amt MONEY,
+    package_credit INTEGER,
     CHECK(refund_amt >= 0),
-	sid INTEGER NOT NULL REFERENCES Sessions(sid),
+    sid INTEGER NOT NULL REFERENCES Sessions(sid),
     cust_id INTEGER NOT NULL REFERENCES Customers(cust_id)
 );
