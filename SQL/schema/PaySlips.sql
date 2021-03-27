726523551ac7c6bf1b2bf4009afa356f21a0891f
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2021-03-27 15:43:11 +0800

    PaySlips: Cascade because it is weak entity

--- SQL/schema/PaySlips.sql
+++ SQL/schema/PaySlips.sql
@@ -1,8 +1,8 @@
 drop table if exists PaySlips cascade;
 create table PaySlips (
-    eid integer references Employees,
+    eid integer references Employees ON DELETE CASCADE ON UPDATE CASCADE,
     payment_date date,
-    amount numeric, 
+    amount numeric,
     num_work_hours integer,
     num_work_days integer,
     primary key (eid, payment_date),
