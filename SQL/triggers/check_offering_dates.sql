035d752393a56597c5c4bd4dae2dfcf9fbac3b95
Wen Junhua <wenjh1998@gmail.com>
2021-03-31 22:56:48 +0800

    Update check_offering_dates.sql
    Update the start date and end date of course offerings based on the sessions inserted, deleted or updated.

--- SQL/triggers/check_offering_dates.sql
+++ SQL/triggers/check_offering_dates.sql
@@ -1,20 +1,28 @@
 CREATE OR REPLACE FUNCTION check_offering_dates() RETURNS TRIGGER AS $$
-DECLARE start_date DATE;
-    end_date DATE BEGIN
-    SELECT MAX(s.end_date),
-        MIN(s.start_date) INTO end_date,
-        start_date
+DECLARE 
+    m_start_date DATE;
+    m_end_date DATE;
+BEGIN
+    SELECT MAX(date),
+        MIN(date) INTO m_end_date,
+        m_start_date
     FROM Sessions s
     WHERE NEW.sid = s.sid
         AND NEW.course_id = s.course_id
         AND NEW.launch_date = s.launch_date;
+
+    UPDATE CourseOffering c SET start_date = m_start_date, end_date = m_end_date
+    WHERE c.course_id = OLD.course_id AND c.launch_date = OLD.launch_date;
 END;
 $$ LANGUAGE PLPGSQL;
-CREATE TRIGGER update_date_trigger
-AFTER INSERT OR UPDATE ON Sessions 
-FOR EACH ROW EXECUTE FUNCTION check_offering_dates()
-WHEN (
-        TG_OP = 'INSERT'
-        OR NEW.start_date <> OLD.start_date
-        OR NEW.end_date <> OLD.end_date
-    );
+
+
+CREATE TRIGGER update_update_date_trigger
+AFTER UPDATE ON Sessions 
+FOR EACH ROW WHEN (NEW.date IS DISTINCT FROM OLD.date)
+EXECUTE FUNCTION check_offering_dates();
+
+
+CREATE TRIGGER update_others_date_trigger
+AFTER INSERT OR DELETE ON Sessions
+FOR EACH ROW EXECUTE FUNCTION check_offering_dates();
