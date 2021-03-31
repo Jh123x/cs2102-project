80efd6f57057899bde22bcd86c6ca85e508dc4e5
Junhua <wenjh1998@gmail.com>
2021-03-31 15:48:33 +0800

    Update update_instructor.sql

--- SQL/functions/update_instructor.sql
+++ SQL/functions/update_instructor.sql
@@ -9,21 +9,21 @@ DECLARE
     session_start_time TIMESTAMP;
     is_valid_instructor INTEGER;
 BEGIN
-    SELECT s.date INTO session_date, s.start_time INTO session_start_time
+    SELECT s.date, s.start_time INTO session_date, session_start_time
     FROM Sessions s
     WHERE sid = s.session_number;
 
-    IF session_start_time >= CURRENT_TIME() THEN
-        RAISE EXCEPTION "Session already started! Cannot update instructor!"
-    ENDIF
+    IF (session_start_time >= CURRENT_TIME) THEN
+        RAISE EXCEPTION 'Session already started! Cannot update instructor!';
+    END IF;
 
     SELECT COUNT(*) INTO is_valid_instructor
     FROM find_instructors(course_id, session_date, session_start_hour)
     WHERE eid = new_instructor_eid;
     IF is_valid_instructor = 0 THEN
-        RAISE EXCEPTION "This instructor cannot teach this session!"
-    ENDIF
+        RAISE EXCEPTION 'This instructor cannot teach this session!';
+    END IF;
 
 
-END
+END;
 $$ LANGUAGE plpgsql
