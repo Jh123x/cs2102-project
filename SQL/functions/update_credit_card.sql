3d541158ff005732d09985377e20c471db0000ac
Junhua <wenjh1998@gmail.com>
2021-03-31 15:50:32 +0800

    Update update_credit_card.sql

--- SQL/functions/update_credit_card.sql
+++ SQL/functions/update_credit_card.sql
@@ -5,7 +5,7 @@ CREATE OR REPLACE PROCEDURE update_credit_card (
     expiry_date DATE
     ) AS $$
 BEGIN
-    INSERT INTO Credit_cards (number, cvv, expiry_date) VALUES (number, cvv, expiry_date);
-    INSERT INTO Owns (cust_id, number, from_date) VALUES (cust_id, number, CURRENT_DATE());
-END
-$$ LANGUAGE SQL
+		INSERT INTO CreditCards (number, cvv, expiry_date) VALUES (number, cvv, expiry_date);
+		INSERT INTO Owns (cust_id, number, from_date) VALUES (cust_id, number, CURRENT_DATE);
+END;
+$$ LANGUAGE plpgsql;
