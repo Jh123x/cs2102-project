92b23194d6aab58313dfa3b88927fef8c6088330
Junhua <wenjh1998@gmail.com>
2021-03-31 14:12:23 +0800

    Add id to PK

--- SQL/schema/CourseOffering.sql
+++ SQL/schema/CourseOffering.sql
@@ -20,7 +20,7 @@ create table CourseOfferings (
     CHECK(fees >= 0),
     CHECK(start_date >= registration_deadline + INTEGER '10'),
 
-    PRIMARY KEY(launch_date, course_id),
+    PRIMARY KEY(launch_date, course_id, id),
     FOREIGN KEY(course_id) REFERENCES Courses on delete cascade,
     FOREIGN KEY(admin_id) REFERENCES Administrators on update cascade
 );
