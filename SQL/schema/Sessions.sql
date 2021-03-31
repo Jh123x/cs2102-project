a08e125ca7ce5305f0a0072876a496d0c660a12d
ninggggx99 <qingning99@gmail.com>
2021-03-31 11:49:25 +0800

    Fix date error

--- SQL/schema/Sessions.sql
+++ SQL/schema/Sessions.sql
@@ -14,7 +14,7 @@ CREATE TABLE Sessions (
     FOREIGN KEY(sid, launch_date, course_id) REFERENCES CourseOfferings(id, launch_date, course_id) ON DELETE CASCADE,
 
     -- Check if the session is conducted between 9am - 12pm to 2pm - 6pm and between
-    CHECK(to_char(join_date, 'Day') IN ('Monday','Tuesday', 'Wednesday','Thursday','Friday')),
+    CHECK(to_char(date, 'Day') IN ('Monday','Tuesday', 'Wednesday','Thursday','Friday')),
     CHECK((9 <= EXTRACT(HOUR FROM start_time) AND EXTRACT(HOUR FROM start_time) <= 12) OR (14 <= EXTRACT(HOUR FROM start_time) AND EXTRACT(HOUR FROM start_time) <= 18)),
     CHECK((9 <= EXTRACT(HOUR FROM end_time) AND EXTRACT(HOUR FROM end_time) <= 12) OR (14 <= EXTRACT(HOUR FROM end_time) AND EXTRACT(HOUR FROM end_time) <= 18)),
     CHECK(end_time > start_time)
