2dfc59538d745d2c51116da19413c6a7d590102e
Junhua <wenjh1998@gmail.com>
2021-03-31 15:46:12 +0800

    Update add_employee.sql

--- SQL/functions/add_employee.sql
+++ SQL/functions/add_employee.sql
@@ -11,55 +11,50 @@ CREATE OR REPLACE PROCEDURE add_employee (
     ) AS $$
 DECLARE
     eid INTEGER;
+	area TEXT;
 BEGIN
-    -- Generate the next eid
-    SELECT MAX(e.eid) + 1 INTO eid FROM Employees;
-    INSERT INTO Employees
-    VALUES (
-            eid,
-            name,
-            address,
-            phone,
-            email,
-            join_date,
-            NULL -- No depart date yet
-        );
+    /* Insert the employee in */
+    INSERT INTO Employees(name, address, phone, email, join_date) VALUES (name, address, phone, email, join_date);
+
+    /* Get the new id that is generated */
+    SELECT e.eid INTO eid FROM Employees WHERE e.name = name AND e.address = address AND e.phone = phone AND e.email = email AND e.join_date = join_date;
 
     -- Add into part-time / full time
     IF (type = 'part-time') THEN
         INSERT INTO PartTimeEmployees VALUES (eid, salary_amount);
-    ELSE IF (type = 'full-time') THEN
+    ELSIF (type = 'full-time') THEN
         INSERT INTO FullTimeEmployees VALUES (eid, salary_amount);
     ELSE
-        RAISE EXCEPTION "Type not found";
+        RAISE EXCEPTION 'Type not found';
     END IF;
 
     -- Add into role specific table
-    IF (category = "Manager") THEN
+    IF (category = 'Manager') THEN
         INSERT INTO Managers VALUES (eid);
 
         -- Add them to the specified course area
-        FOR area IN course_area
+        FOR area IN (SELECT * FROM course_area)
         LOOP
             INSERT INTO Course_areas VALUES (area, eid);
         END LOOP;
-    ELSE IF (category = "Admin") THEN
+    ELSIF (category = 'Admin') THEN
         INSERT INTO Administrators VALUES (eid);
         IF (course_area.COUNT > 0) THEN
-            RAISE EXCEPTION "Admin should not have course area";
-    ELSE IF (category = "Instructor") THEN
+            RAISE EXCEPTION 'Admin should not have course area';
+		END IF;
+    ELSIF (category = 'Instructor') THEN
         INSERT INTO Instructors VALUES (eid);
-        FOR area IN course_area
+        FOR area IN (SELECT * FROM course_area)
         LOOP
             INSERT INTO Specializes VALUES (eid, area);
         END LOOP;
-        IF (type = "part-time") THEN
+        IF (type = 'part-time') THEN
             INSERT INTO PartTimeInstructors VALUES (eid);
-        ELSE IF (type = 'full-time') THEN
+        ELSIF (type = 'full-time') THEN
             INSERT INTO FullTimeInstructors VALUES (eid);
         END IF;
     ELSE
-        RAISE EXCEPTION "Category not found";
+        RAISE EXCEPTION 'Category not found';
     END IF;
 END;
 $$ LANGUAGE plpgsql;
