fb19399dfe9988eff021fc6c6bc85cab7aa6f7b6
Creastery <Creastery@users.noreply.github.com>
2021-04-01 17:24:20 +0800

    Fixed drop triggers again oops

--- SQL/triggers/drop_all_triggers.sql
+++ SQL/triggers/drop_all_triggers.sql
@@ -1,21 +1,27 @@
 /*
     Update this file using this command:
-    grep -RihoPz '(?s)(?<=create )trigger .+?\n.+?\n' --exclude drop* | awk 'NF>1{printf "DROP %s IF EXISTS ", $0; if (NF != 0) {getline; printf "ON %s CASCADE;\n", $NF;}}' && grep -RihoP '(function|procedure) .+(?=\()' | awk '{print "DROP", $1, "IF EXISTS", $2, "CASCADE;"}' | uniq
+    grep -RihoPz '(?s)create (constraint )?trigger .+?\n.+?\n' --exclude drop* | awk 'NF>1{printf "DROP TRIGGER IF EXISTS %s ", $NF; if (NF != 0) {getline; printf "ON %s CASCADE;\n", $NF;}}' && grep -RihoP '(function|procedure) .+(?=\()' | awk '{print "DROP", $1, "IF EXISTS", $2, "CASCADE;"}' | uniq
 */
-DROP TRIGGER customer_redeems_trigger IF EXISTS ON Redeems CASCADE;
-DROP TRIGGER update_update_date_trigger IF EXISTS ON Sessions CASCADE;
-DROP TRIGGER update_others_date_trigger IF EXISTS ON Sessions CASCADE;
-DROP TRIGGER customer_redeems_trigger IF EXISTS ON Redeems CASCADE;
-DROP TRIGGER customer_register_trigger IF EXISTS ON Registers CASCADE;
-DROP TRIGGER admin_check_trigger IF EXISTS ON Administrators CASCADE;
-DROP TRIGGER manager_check_trigger IF EXISTS ON Managers CASCADE;
-DROP TRIGGER instructor_check_trigger IF EXISTS ON Instructors CASCADE;
-DROP TRIGGER no_delete_employee_trigger IF EXISTS ON Employees CASCADE;
-DROP TRIGGER part_time_insert_trigger IF EXISTS ON PartTimeEmployees CASCADE;
-DROP TRIGGER full_time_insert_trigger IF EXISTS ON FullTimeEmployees CASCADE;
-DROP TRIGGER not_more_than_30_trigger IF EXISTS ON Sessions CASCADE;
-DROP TRIGGER redemption_check_trigger IF EXISTS ON Redeems CASCADE;
-DROP TRIGGER session_collision_trigger IF EXISTS ON Sessions CASCADE;
+
+DROP TRIGGER IF EXISTS customer_redeems_trigger ON Redeems CASCADE;
+DROP TRIGGER IF EXISTS update_update_date_trigger ON Sessions CASCADE;
+DROP TRIGGER IF EXISTS update_others_date_trigger ON Sessions CASCADE;
+DROP TRIGGER IF EXISTS customers_total_participation ON Customers CASCADE;
+DROP TRIGGER IF EXISTS credit_cards_key_constraint ON CreditCards CASCADE;
+DROP TRIGGER IF EXISTS credit_cards_total_participation_constraint ON CreditCards CASCADE;
+DROP TRIGGER IF EXISTS customers_total_participation_constraint ON Owns CASCADE;
+DROP TRIGGER IF EXISTS credit_cards_key_constraint ON Owns CASCADE;
+DROP TRIGGER IF EXISTS customer_redeems_trigger ON Redeems CASCADE;
+DROP TRIGGER IF EXISTS customer_register_trigger ON Registers CASCADE;
+DROP TRIGGER IF EXISTS admin_check_trigger ON Administrators CASCADE;
+DROP TRIGGER IF EXISTS manager_check_trigger ON Managers CASCADE;
+DROP TRIGGER IF EXISTS instructor_check_trigger ON Instructors CASCADE;
+DROP TRIGGER IF EXISTS no_delete_employee_trigger ON Employees CASCADE;
+DROP TRIGGER IF EXISTS part_time_insert_trigger ON PartTimeEmployees CASCADE;
+DROP TRIGGER IF EXISTS full_time_insert_trigger ON FullTimeEmployees CASCADE;
+DROP TRIGGER IF EXISTS not_more_than_30_trigger ON Sessions CASCADE;
+DROP TRIGGER IF EXISTS redemption_check_trigger ON Redeems CASCADE;
+DROP TRIGGER IF EXISTS session_collision_trigger ON Sessions CASCADE;
 DROP FUNCTION IF EXISTS customer_redeems_check CASCADE;
 DROP FUNCTION IF EXISTS check_offering_dates CASCADE;
 DROP FUNCTION IF EXISTS customers_total_participation_check CASCADE;
