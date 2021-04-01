b7331fa00b0ad44f3b965bcf1c95d8c6331872a5
Wen Junhua <wenjh1998@gmail.com>
2021-04-01 12:44:48 +0800

    Create package_status_view

--- SQL/views/CoursePackageStatus.sql
+++ SQL/views/CoursePackageStatus.sql
@@ -1,1 +1,20 @@
 /*Create view for status of course package*/
+CREATE OR REPLACE VIEW package_status AS
+SELECT b.customer_id, 
+    b.date, 
+    (SELECT 
+	 CASE 
+      WHEN b.num_remaining_redemptions > 0 THEN 'Active'
+      WHEN (
+        SELECT COUNT(*) 
+        FROM Redeems r JOIN Sessions s 
+        ON r.session_id = s.session_id 
+        AND r.course_id = s.course_id 
+        AND r.launch_date = r.launch_date
+        WHERE r.customer_id = b.customer_id 
+        AND s.date > CURRENT_DATE
+        ) > 0 THEN 'Partially Active'
+      ELSE 'Inactive'
+    END)
+FROM Buys b
+GROUP BY customer_id, date;
