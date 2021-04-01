9c5faf5a4baf2822a74f02fcf0ec8006a83325f2
Creastery <Creastery@users.noreply.github.com>
2021-04-01 21:21:04 +0800

    Fixed add_course_package function & CoursePackage schema

--- SQL/functions/add_course_package.sql
+++ SQL/functions/add_course_package.sql
@@ -1,22 +1,21 @@
-CREATE OR REPLACE PROCEDURE add_course_package (
-    package_id INTEGER,
-    package_sale_start_date DATE,
-    package_num_free_registrations INTEGER,
-    package_sale_end_date DATE,
+CREATE OR REPLACE FUNCTION add_course_package (
     package_name TEXT,
-    package_price DEC(65, 2),
-    package_date DATE /* What is this? */
-) AS $$
+    package_num_free_registrations INTEGER,
+    package_sale_start_date DATE,
+    package_sale_end_date DATE,
+    package_price DEC(65, 2)
+)
+RETURNS TABLE (package_id INTEGER) AS $$
+DECLARE
+    new_package RECORD;
 BEGIN
     INSERT INTO CoursePackages
-    VALUES (
-        package_id,
-        package_sale_start_date,
-        package_num_free_registrations,
-        package_sale_end_date,
-        package_name,
-        package_price,
-        package_date /* this does not even exist in CoursePackages */
-    );
+    (package_sale_start_date, package_num_free_registrations, package_sale_end_date, package_name, package_price)
+    VALUES
+    (package_sale_start_date, package_num_free_registrations, package_sale_end_date, package_name, package_price)
+    RETURNING * INTO new_package;
+
+    package_id := new_package.package_id;
+    RETURN NEXT;
 END
 $$ LANGUAGE plpgsql;
--- SQL/schema/CoursePackages.sql
+++ SQL/schema/CoursePackages.sql
@@ -1,12 +1,13 @@
 DROP TABLE IF EXISTS CoursePackages CASCADE;
 CREATE TABLE CoursePackages (
     package_id SERIAL PRIMARY KEY,
-    package_sale_start_date DATE NOT NULL,
+    package_name TEXT NOT NULL,
     package_num_free_registrations INTEGER NOT NULL,
+    package_sale_start_date DATE NOT NULL,
     package_sale_end_date DATE NOT NULL,
-    package_name TEXT unique NOT NULL,
     package_price DEC(64,2) NOT NULL,
 
     CHECK(package_price >= 0),
-    CHECK(package_num_free_registrations >= 0)
+    CHECK(package_num_free_registrations >= 0),
+    CHECK(package_sale_start_date <= package_sale_end_date)
 );
