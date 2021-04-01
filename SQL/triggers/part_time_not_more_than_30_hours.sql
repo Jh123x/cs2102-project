b635cf6e4295455744ff041b6d1d78aa0f94c9b0
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2021-04-01 23:17:19 +0800

    30 hours req: consider sessions in month of new session

--- SQL/triggers/part_time_not_more_than_30_hours.sql
+++ SQL/triggers/part_time_not_more_than_30_hours.sql
@@ -5,8 +5,9 @@ DECLARE
 BEGIN
     SELECT COALESCE(SUM(session_end_hour - session_start_hour), 0) INTO hours
     FROM Sessions
-    WHERE instructor_id = NEW.instructor_id AND EXTRACT(MONTH FROM session_date) == EXTRACT(MONTH FROM CURRENT_DATE);
+    WHERE instructor_id = NEW.instructor_id AND EXTRACT(MONTH FROM session_date) == EXTRACT(MONTH FROM NEW.session_date);
     /* btw what's with the month check ^? */
+    /* because limited to 30 hours in the same month? instead of entire career? */
 
     IF (hours > 30) THEN
         RAISE EXCEPTION 'Part time Employee working too much';
