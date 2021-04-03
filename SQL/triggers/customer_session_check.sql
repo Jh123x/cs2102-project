b6cfb660eca9a66b92d11bbdb6237e1c997ec7c9
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2021-04-03 15:37:51 +0800

    Disallow session registration if registration deadline has passed

--- SQL/triggers/customer_session_check.sql
+++ SQL/triggers/customer_session_check.sql
@@ -1,12 +1,21 @@
 CREATE OR REPLACE FUNCTION customer_session_check()
 RETURNS TRIGGER AS $$
 DECLARE
-    register_count INTEGER;
-    redeem_count INTEGER;
-    cancel_count INTEGER;
+    registration_deadline   DATE;
+    register_count          INTEGER;
+    redeem_count            INTEGER;
+    cancel_count            INTEGER;
 BEGIN
     IF (TG_OP = 'UPDATE' AND NEW.customer_id = OLD.customer_id AND NEW.course_id = OLD.course_id) THEN
         RETURN NEW;
+    END IF;
+
+    SELECT c.offering_registration_deadline INTO registration_deadline
+    FROM CourseOfferings c
+    WHERE c.course_id = NEW.course_id AND c.offering_launch_date = NEW.offering_launch_date;
+
+    IF CURRENT_DATE > registration_deadline THEN
+        RAISE EXCEPTION 'Registration deadline for this session is over.'
     END IF;
 
     SELECT COUNT(*) INTO register_count FROM Registers WHERE NEW.customer_id = customer_id AND NEW.course_id = course_id;
