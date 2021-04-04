0606ad6b4ef2e30a3bff338891d76d13971f1968
Wen Junhua <wenjh1998@gmail.com>
2021-04-04 11:42:09 +0800

    Fix Syntax Errors

--- SQL/functions/remove_employee.sql
+++ SQL/functions/remove_employee.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE PROCEDURE remove_employee(
-    employee_id INTEGER,
+    r_employee_id INTEGER,
     departure_date DATE
 ) AS $$
 DECLARE
@@ -9,21 +9,21 @@ DECLARE
 BEGIN
     /* The below conditions needs changing; need to check if it's managing a course offering/session AFTER departure date */
     /* Check if they are still handling admin tasks */
-    SELECT COUNT(*) INTO isAdmin_count FROM CourseOfferings c WHERE c.admin_id = employee_id;
+    SELECT COUNT(*) INTO isAdmin_count FROM CourseOfferings c WHERE c.admin_id = r_employee_id;
 
     /* Check if they are still teaching some course */
-    SELECT COUNT(*) INTO isManaging_count FROM Course_areas c WHERE c.manager_id = employee_id;
+    SELECT COUNT(*) INTO isManaging_count FROM CourseAreas c WHERE c.manager_id = r_employee_id;
 
     /* Check if they are manager managing some area */
-    SELECT COUNT(*) INTO isTeaching_count FROM Sessions s WHERE s.instructor_id = employee_id;
+    SELECT COUNT(*) INTO isTeaching_count FROM Sessions s WHERE s.instructor_id = r_employee_id;
 
     IF (isAdmin_count + isManaging_count + isTeaching_count > 0) THEN
         RAISE EXCEPTION 'Employee is still Admin/Managing/Teaching';
     END IF;
 
     /* Leave it to insert/update CHECK() to ensure departure_date is >= join_date*/
-    UPDATE Employees
+    UPDATE Employees e
     SET employee_depart_date = departure_date
-    WHERE e.employee_id = employee_id;
+    WHERE e.employee_id = r_employee_id;
 END;
 $$ LANGUAGE plpgsql;
