8cfaef9a7cb9c8a5eb20976ddf36552411aba647
Creastery <Creastery@users.noreply.github.com>
2021-04-04 23:19:10 +0800

    Added missing check for existing active package in buy_course_package()

--- SQL/functions/buy_course_package.sql
+++ SQL/functions/buy_course_package.sql
@@ -2,37 +2,41 @@
     13. buy_course_package: This routine is used when a customer requests to purchase a course package.
     The inputs to the routine include the customer and course package identifiers.
     If the purchase transaction is valid, the routine will process the purchase with the necessary updates (e.g., payment).
-
-    Todo: Need to check if customer has no active/partially active course package first.
 */
 DROP FUNCTION IF EXISTS buy_course_package;
 CREATE OR REPLACE FUNCTION buy_course_package (
-    r_customer_id         INTEGER,
-    r_package_id          INTEGER
+    customer_id_arg         INTEGER,
+    package_id_arg          INTEGER
 ) RETURNS VOID
 AS $$
 DECLARE
     package_sale_start_date         DATE;
     package_sale_end_date           DATE;
     package_num_free_registrations  INTEGER;
-    r_credit_card_number            CHAR(16);
+    credit_card_number            CHAR(16);
 BEGIN
     /* Select last owned credit card of customer */
-    SELECT credit_card_number INTO r_credit_card_number
+    SELECT o.credit_card_number INTO credit_card_number
     FROM Owns o
-    WHERE o.customer_id = r_customer_id
+    WHERE o.customer_id = customer_id_arg
     ORDER BY o.own_from_date DESC
     LIMIT 1;
     
-    IF r_credit_card_number IS NULL THEN
-        RAISE EXCEPTION 'There is no credit card found for customer. Check if customer_id supplied is valid.';
+    IF credit_card_number IS NULL THEN
+        RAISE EXCEPTION 'There is no credit card found for customer. Check if customer_id supplied (%) is valid.', customer_id_arg;
+    END IF;
+
+    /* Check if customer has an active/partially active package now */
+    IF customer_has_active_ish_course_package(customer_id_arg) IS TRUE
+    THEN
+        RAISE EXCEPTION 'Customer % cannot purchase a course package as there is an active/partially active course package.', customer_id_arg;
     END IF;
 
     /* Check if course package is still for sale */
     SELECT cp.package_sale_start_date, cp.package_sale_end_date, cp.package_num_free_registrations
         INTO package_sale_start_date, package_sale_end_date, package_num_free_registrations
     FROM CoursePackages cp
-    WHERE cp.package_id = r_package_id;
+    WHERE cp.package_id = package_id_arg;
 
     IF CURRENT_DATE < package_sale_start_date THEN
         RAISE EXCEPTION 'This package is not for sale yet.';
@@ -44,6 +48,6 @@ BEGIN
     INSERT INTO Buys
     (buy_date, buy_num_remaining_redemptions, package_id, customer_id, credit_card_number)
     VALUES
-    (CURRENT_DATE, package_num_free_registrations, r_package_id, r_customer_id, r_credit_card_number);
+    (CURRENT_DATE, package_num_free_registrations, package_id_arg, customer_id_arg, credit_card_number);
 END;
 $$ LANGUAGE plpgsql;
--- SQL/functions/get_my_course_package.sql
+++ SQL/functions/get_my_course_package.sql
@@ -12,6 +12,95 @@
         ascending order of session date and
         start hour.
 */
+DROP FUNCTION IF EXISTS customer_has_active_ish_course_package CASCADE;
+CREATE OR REPLACE FUNCTION customer_has_active_ish_course_package (
+    customer_id_arg INTEGER
+)
+RETURNS BOOLEAN AS $$
+BEGIN
+    RETURN EXISTS(
+        SELECT * FROM get_customer_active_ish_course_package(customer_id_arg) AS cp
+    );
+END;
+$$ LANGUAGE PLPGSQL;
+
+DROP FUNCTION IF EXISTS get_customer_active_ish_course_package CASCADE;
+CREATE OR REPLACE FUNCTION get_customer_active_ish_course_package (
+    customer_id_arg INTEGER
+)
+RETURNS TABLE(
+    buy_date TIMESTAMP,
+    package_name TEXT,
+    package_price DEC(64,2),
+    package_num_free_registrations INTEGER,
+    buy_num_remaining_redemptions INTEGER
+) AS $$
+BEGIN
+    RETURN QUERY (
+        /*
+            Active - at least one unused session in the package
+            Partially active - at least one cancellable session (at least 7 days before session date)
+        */
+        SELECT cp.buy_date, cp.package_name, cp.package_price, cp.package_num_free_registrations, cp.buy_num_remaining_redemptions
+        FROM get_customer_course_packages(customer_id_arg) AS cp
+        WHERE cp.buy_num_remaining_redemptions > 1
+            OR EXISTS(
+                SELECT redeem_date
+                FROM Redeems r
+                NATURAL JOIN Buys b
+                NATURAL JOIN Sessions s
+                WHERE r.buy_date = cp.buy_date AND s.session_date >= CURRENT_DATE + 7
+            )
+        ORDER BY cp.buy_date DESC
+        LIMIT 1
+    );
+END;
+$$ LANGUAGE PLPGSQL;
+
+DROP FUNCTION IF EXISTS customer_has_course_packages CASCADE;
+CREATE OR REPLACE FUNCTION customer_has_course_packages (
+    customer_id_arg INTEGER
+)
+RETURNS BOOLEAN AS $$
+BEGIN
+    RETURN EXISTS(
+        SELECT * FROM get_customer_course_packages(customer_id_arg) AS cp
+    );
+END;
+$$ LANGUAGE PLPGSQL;
+
+DROP FUNCTION IF EXISTS get_customer_course_packages CASCADE;
+CREATE OR REPLACE FUNCTION get_customer_course_packages (
+    customer_id_arg INTEGER
+)
+RETURNS TABLE(
+    buy_date TIMESTAMP,
+    package_name TEXT,
+    package_price DEC(64,2),
+    package_num_free_registrations INTEGER,
+    buy_num_remaining_redemptions INTEGER
+) AS $$
+BEGIN
+    RETURN QUERY (
+        SELECT b.buy_date, cp.package_name, cp.package_price, cp.package_num_free_registrations, b.buy_num_remaining_redemptions
+        FROM Buys b
+        NATURAL JOIN CoursePackages cp
+        WHERE b.customer_id = customer_id_arg
+        ORDER BY buy_date DESC
+    );
+END;
+$$ LANGUAGE PLPGSQL;
+
+DROP FUNCTION IF EXISTS has_active_or_partially_active_course_package CASCADE;
+CREATE OR REPLACE FUNCTION has_active_or_partially_active_course_package (
+    customer_id_arg INTEGER
+)
+RETURNS BOOLEAN AS $$
+BEGIN
+
+END;
+$$ LANGUAGE PLPGSQL;
+
 DROP FUNCTION IF EXISTS get_my_course_package CASCADE;
 CREATE OR REPLACE FUNCTION get_my_course_package (
     customer_id_arg INTEGER
@@ -25,58 +114,42 @@ DECLARE
     buy_num_remaining_redemptions INTEGER;
     redeemed_sessions JSON;
 BEGIN
-    SELECT b.buy_date, cp.package_name, cp.package_price, cp.package_num_free_registrations, b.buy_num_remaining_redemptions
-    INTO cp_buy_date, package_name, package_price, package_num_free_registrations, buy_num_remaining_redemptions
-    FROM Buys b
-    NATURAL JOIN CoursePackages cp
-    WHERE b.customer_id = customer_id_arg
-    ORDER BY buy_date DESC
-    LIMIT 1;
-
-    IF cp_buy_date IS NULL
+    IF customer_has_course_packages(customer_id_arg) IS NOT TRUE
     THEN
         RAISE NOTICE 'Customer % has not purchased any course packages.', customer_id_arg;
         course_package_details := row_to_json(row());
-    ELSE
-        /*
-            Must have either active or partially active course package
-            Active - at least one unused session in the package
-            Partially active - at least one cancellable session (at least 7 days before session date)
-        */
-        IF buy_num_remaining_redemptions > 1 OR EXISTS(
-            SELECT redeem_date
+    /* Customer must have either active or partially active course package */
+    ELSIF customer_has_active_ish_course_package(customer_id_arg) IS TRUE
+    THEN
+        SELECT cp.buy_date, cp.package_name, cp.package_price, cp.package_num_free_registrations, cp.buy_num_remaining_redemptions
+        INTO cp_buy_date, package_name, package_price, package_num_free_registrations, buy_num_remaining_redemptions
+        FROM get_customer_active_ish_course_package(customer_id_arg) AS cp;
+
+        /* Aggregate all sessions redeemed using course package sorted in ascending order of session date and start hour */
+        SELECT COALESCE(json_agg(session_information), '[]'::JSON) INTO redeemed_sessions
+        FROM (
+            SELECT c.course_title, s.session_date, s.session_start_hour
             FROM Redeems r
             NATURAL JOIN Buys b
             NATURAL JOIN Sessions s
-            WHERE r.buy_date = cp_buy_date AND s.session_date >= CURRENT_DATE + 7
-        )
-        THEN
-            /* Aggregate all sessions redeemed using course package sorted in ascending order of session date and start hour */
-            SELECT COALESCE(json_agg(session_information), '[]'::JSON) INTO redeemed_sessions
-            FROM (
-                SELECT c.course_title, s.session_date, s.session_start_hour
-                FROM Redeems r
-                NATURAL JOIN Buys b
-                NATURAL JOIN Sessions s
-                NATURAL JOIN CourseOfferings co
-                NATURAL JOIN Courses c
-                WHERE r.buy_date = cp_buy_date AND r.redeem_cancelled IS NOT TRUE
-                ORDER BY s.session_date ASC, s.session_start_hour ASC
-            ) AS session_information;
+            NATURAL JOIN CourseOfferings co
+            NATURAL JOIN Courses c
+            WHERE r.buy_date = cp_buy_date AND r.redeem_cancelled IS NOT TRUE
+            ORDER BY s.session_date ASC, s.session_start_hour ASC
+        ) AS session_information;
 
-            /* Return value is JSON object */
-            SELECT jsonb_build_object(
-                'buy_date', cp_buy_date,
-                'package_name', package_name,
-                'package_price', package_price,
-                'package_num_free_registrations', package_num_free_registrations,
-                'buy_num_remaining_redemptions', buy_num_remaining_redemptions,
-                'redeemed_sessions', redeemed_sessions
-            ) INTO course_package_details;
-        ELSE
-            RAISE NOTICE 'Customer % has no active or partially active course packages.', customer_id_arg;
-            course_package_details := row_to_json(row());
-        END IF;
+        /* Return value is JSON object */
+        SELECT jsonb_build_object(
+            'buy_date', cp_buy_date,
+            'package_name', package_name,
+            'package_price', package_price,
+            'package_num_free_registrations', package_num_free_registrations,
+            'buy_num_remaining_redemptions', buy_num_remaining_redemptions,
+            'redeemed_sessions', redeemed_sessions
+        ) INTO course_package_details;
+    ELSE
+        RAISE NOTICE 'Customer % has no active or partially active course packages.', customer_id_arg;
+        course_package_details := row_to_json(row());
     END IF;
     RETURN NEXT;
 END;
