71b5bd8162b10c15e4016c0cd669bddc7ebc84e4
Wen Junhua <wenjh1998@gmail.com>
2021-04-04 12:24:26 +0800

    Add unittest for remove_employee

---
+++ SQL/testfiles/testclass/remove_employee_test.py
@@ -0,0 +1,61 @@
+import unittest
+import datetime
+from .basetest import BaseTest
+from psycopg2.errors import RaiseException
+
+
+class RemoveEmployeeTest(BaseTest, unittest.TestCase):
+
+    def _add_person(self, role: str, course_areas: list = 'ARRAY[]::TEXT[]') -> None:
+        """Add a manager into the table"""
+        args = ["John", "address", '987654321', 'test@test.com',
+                '2020-05-03', role, "full-time", '10.5', str(course_areas)]
+        manager_query = self.generate_query("add_employee", tuple(args))
+        self.execute_query(manager_query)
+
+    def test_remove_manager(self) -> None:
+        """Remove the manager"""
+        # Add a manager in
+        self._add_person("Manager")
+
+        # Check if the manager is added
+        out = self.execute_query('SELECT * FROM Employees WHERE employee_id = 12')
+        assert len(out) == 1, out
+
+        args = ('12', '2020-12-21')
+        query = self.generate_procedure('remove_employee', args)
+        res = self.execute_query(query)
+
+        # Nothing should be returned at the cursor
+        assert res == None
+
+        # Check if there are any employees left
+        expected = (12, "John", "address", '987654321', 'test@test.com', datetime.datetime.strptime("2020-05-03", "%Y-%m-%d"), datetime.datetime.strptime("2020-12-21", "%Y-%m-%d"))
+        self.value_test('SELECT * FROM Employees WHERE employee_id = 12', expected)
+
+
+    def test_remove_non_existent(self) -> None:
+        """Remove someone who does not exist"""
+
+        # Remove someone who is not in the list
+        args = ['-3', '2020-04-21']
+        query = self.generate_procedure('remove_employee', tuple(args))
+        self.check_fail_test(query, "Test is suppose to fail", (RaiseException,))
+
+    def test_remove_manager_managing(self) -> None:
+        """Remove a manager who has a role"""
+        self._add_person('Manager', "ARRAY['Database']")
+        
+        # Check if the manager is added
+        out = self.execute_query('SELECT * FROM Employees WHERE employee_id = 13')
+        assert len(out) == 1, out
+
+        args = ('13', '2020-12-21')
+        query = self.generate_procedure('remove_employee', args)
+        res = self.check_fail_test(query, "Not suppose to be able to remove a manager that is managing", (RaiseException,))
+
+        # Nothing should be returned at the cursor
+        assert res == None
+
+        
+    
