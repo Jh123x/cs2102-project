e82842c10882a3e1437b94f2d6464e85a5c63a37
Wen Junhua <wenjh1998@gmail.com>
2021-04-04 23:38:18 +0800

    Update ordering

--- SQL/testfiles/testclass/__init__.py
+++ SQL/testfiles/testclass/__init__.py
@@ -6,5 +6,6 @@ from .add_course_test import CAddCourseTest
 from .add_customer_test import DAddCustomerTest
 from .update_credit_card_test import EUpdateCreditCardTest
 from .find_instructors_test import FindInstructorsTest
-from .add_course_offering_test import GAddCourseOfferingTest
+from .add_course_package_test import GAddCoursePackageTest
+from .add_course_offering_test import HAddCourseOfferingTest
 from .buy_course_package_test import ZBuyCoursePackageTest
--- SQL/testfiles/testclass/add_course_offering_test.py
+++ SQL/testfiles/testclass/add_course_offering_test.py
@@ -2,7 +2,7 @@ import unittest
 from . import BaseTest
 from psycopg2.errors import UniqueViolation
 
-class GAddCourseOfferingTest(BaseTest, unittest.TestCase):
+class HAddCourseOfferingTest(BaseTest, unittest.TestCase):
 
     def test_pass(self):
-        
+        pass
