d76f93e35c1e77910933d01c666b29bb72a13052
Wen Junhua <wenjh1998@gmail.com>
2021-04-06 21:14:08 +0800

    Add unit test for get_available_course_packages
    Add 1 unit test and 2 possible unit test for the future

--- SQL/testfiles/testclass/__init__.py
+++ SQL/testfiles/testclass/__init__.py
@@ -10,4 +10,5 @@ from .add_course_package_test import GAddCoursePackageTest
 from .add_course_offering_test import HAddCourseOfferingTest
 from .get_available_instructors_test import IGetAvailableInstrutors
 from .find_rooms_test import ZFindRoomsTest
+from .get_available_course_packages_test import ZGetAvailableCoursePackages
 from .buy_course_package_test import ZBuyCoursePackageTest
