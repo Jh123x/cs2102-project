c440f43cceba3d18cea56c345fb7c0ab80ca423a
ninggggx99 <qingning99@gmail.com>
2021-04-06 23:25:58 +0800

    add course offering test case

--- SQL/testfiles/testclass/add_course_offering_test.py
+++ SQL/testfiles/testclass/add_course_offering_test.py
@@ -91,11 +91,12 @@ class HAddCourseOfferingTest(BaseTest, unittest.TestCase):
         # Add courses
         self.course_id = self._add_course("Database", "Database")
         self.course_id1 = self._add_course("Network", "Network")
+        self.course_id2 = self._add_course("PSQL", "Database")
 
         self.instructor_ids = {}
-        specialization = (("Database",), ("Network", "Database"), ("Network",))
+        specialization = (("Database",), ("Network",))
         part_full_time = ("Part-Time", "Full-time")
-        for index in range(5):
+        for index in range(3):
             spec = specialization[index % len(specialization)]
             t = part_full_time[index % 2]
             self.instructor_ids[index] = self._add_instructor(
@@ -126,13 +127,13 @@ class HAddCourseOfferingTest(BaseTest, unittest.TestCase):
         expected =[(f"(2021-04-05,100.00,2021-04-20,40,40,{self.course_id},{self.admin_id},2021-05-04,2021-06-04)",)]
         assert res == expected, f'\nOutput:   {res}\nExpected: {expected}'
 
-        arr = self.make_session_array([('2021-05-06', '14', self.rid), ('2021-06-04', '14', self.rid)])
+        arr = self.make_session_array([('2021-05-06', '14', self.rid), ('2021-06-03', '14', self.rid)])
         args = ('2021-04-06','100.00', arr, '2021-04-20', '40', str(self.course_id1), str(self.admin_id))
         query = self.generate_query("add_course_offering", args)
         res = self.execute_query(query)
         assert len(res) > 0, "Course Offering 1 not added successfully"
 
-        expected =[(f"(2021-04-06,100.00,2021-04-20,40,40,{self.course_id1},{self.admin_id},2021-05-06,2021-06-04)",)]
+        expected =[(f"(2021-04-06,100.00,2021-04-20,40,40,{self.course_id1},{self.admin_id},2021-05-06,2021-06-03)",)]
         assert res == expected, f'\nOutput:   {res}\nExpected: {expected}'
 
 
@@ -149,3 +150,77 @@ class HAddCourseOfferingTest(BaseTest, unittest.TestCase):
         assert res == expected, f'Output: {res}\nExpected: {expected}'
 
         res = self.check_fail_test(query, "Adding the same offering should fail", RaiseException)
+
+    def test_add_course_offering_session_on_weekends_fail(self):
+        # Add the course offering 1 time
+        arr = self.make_session_array([('2021-05-09', '14', self.rid1), ('2021-06-12', '14', self.rid1)])
+        args = ('2021-04-07','100.00', arr,'2021-04-20', '40', str(self.course_id), str(self.admin_id))
+        query = self.generate_query("add_course_offering", args)
+        res = self.check_fail_test(query, "Adding the offering with session on weekend should fail", RaiseException)
+    
+    def test_add_course_offering_session_between_lunch_fail(self):
+        # Add the course offering 1 time
+        arr = self.make_session_array([('2021-05-09', '10', self.rid1), ('2021-06-12', '10', self.rid1)])
+        args = ('2021-04-07','100.00', arr,'2021-04-20', '40', str(self.course_id), str(self.admin_id))
+        query = self.generate_query("add_course_offering", args)
+        res = self.check_fail_test(query, "Session time is out of range", RaiseException)
+    
+    def test_add_course_offering_session_room_in_use_fail(self):
+        arr = self.make_session_array([('2021-05-06', '14', self.rid), ('2021-06-03', '14', self.rid)])
+        args = ('2021-04-06','100.00', arr, '2021-04-20', '40', str(self.course_id1), str(self.admin_id))
+        query = self.generate_query("add_course_offering", args)
+        res = self.execute_query(query)
+        assert len(res) > 0, "Course Offering 1 not added successfully"
+
+        expected =[(f"(2021-04-06,100.00,2021-04-20,40,40,{self.course_id1},{self.admin_id},2021-05-06,2021-06-03)",)]
+        assert res == expected, f'\nOutput:   {res}\nExpected: {expected}'
+
+        # Add the course offering 1 time
+        arr = self.make_session_array([('2021-05-06', '14', self.rid), ('2021-06-03', '14', self.rid)])
+        args = ('2021-04-06','100.00', arr, '2021-04-20', '40', str(self.course_id), str(self.admin_id))
+        query = self.generate_query("add_course_offering", args)
+        res = self.check_fail_test(query, "Adding the offering with session room in use should fail", RaiseException)
+
+    def test_add_course_offering_session_target_registration_more_than_capacity_fail(self):
+        # Add the course offering 1 time
+        arr = self.make_session_array([('2021-05-06', '14', self.rid), ('2021-06-03', '14', self.rid)])
+        args = ('2021-04-06','100.00', arr, '2021-04-20', '60', str(self.course_id), str(self.admin_id))
+        query = self.generate_query("add_course_offering", args)
+        res = self.check_fail_test(query, "Adding the offering with number of target registration more than room capacity should fail", RaiseException)
+
+    def test_add_course_offering_session_not_enough_instructors_fail(self):
+        arr = self.make_session_array([('2021-05-06', '14', self.rid), ('2021-06-03', '14', self.rid)])
+        args = ('2021-04-06','100.00', arr, '2021-04-20', '40', str(self.course_id), str(self.admin_id))
+        query = self.generate_query("add_course_offering", args)
+        res = self.execute_query(query)
+        assert len(res) > 0, "Course Offering 1 not added successfully"
+
+        expected =[(f"(2021-04-06,100.00,2021-04-20,40,40,{self.course_id},{self.admin_id},2021-05-06,2021-06-03)",)]
+        assert res == expected, f'\nOutput:   {res}\nExpected: {expected}'
+        
+        # Add the course offering 1 time
+        arr = self.make_session_array([('2021-05-06', '14', self.rid), ('2021-06-03', '14', self.rid)])
+        args = ('2021-04-05','100.00', arr, '2021-04-20', '40', str(self.course_id2), str(self.admin_id))
+        query = self.generate_query("add_course_offering", args)
+        res = self.check_fail_test(query, "Adding the offering with sessions that has not enough instructors should fail", RaiseException)
+
+    def test_add_course_offering_launch_date_after_registration_deadline_fail(self):
+        # Add the course offering 1 time
+        arr = self.make_session_array([('2021-05-06', '14', self.rid), ('2021-06-03', '14', self.rid)])
+        args = ('2021-04-22','100.00', arr, '2021-04-20', '40', str(self.course_id2), str(self.admin_id))
+        query = self.generate_query("add_course_offering", args)
+        res = self.check_fail_test(query, "Adding the offering with launch date after registration deadline should fail", RaiseException)
+
+    def test_add_course_offering_num_target_registration_negative_fail(self):
+        # Add the course offering 1 time
+        arr = self.make_session_array([('2021-05-06', '14', self.rid), ('2021-06-03', '14', self.rid)])
+        args = ('2021-04-22','100.00', arr, '2021-04-20', '-12', str(self.course_id2), str(self.admin_id))
+        query = self.generate_query("add_course_offering", args)
+        res = self.check_fail_test(query, "Adding the offering with launch date after registration deadline should fail", RaiseException)
+
+    def test_add_course_offering_start_date_before_registration_fail(self):
+        # Add the course offering 1 time
+        arr = self.make_session_array([('2021-05-06', '14', self.rid), ('2021-06-03', '14', self.rid)])
+        args = ('2021-04-06','100.00', arr, '2021-04-27', '40', str(self.course_id1), str(self.admin_id))
+        query = self.generate_query("add_course_offering", args)
+        res = self.check_fail_test(query, "Adding the offering with launch date after registration deadline should fail", RaiseException)
