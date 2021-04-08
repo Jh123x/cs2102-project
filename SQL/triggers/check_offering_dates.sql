d030a75bad78576205d73a767b5cc3d34087f0c8
Wen Junhua <wenjh1998@gmail.com>
2021-04-08 20:56:43 +0800

    Update trigger to take care of update delete and insert

--- SQL/triggers/check_offering_dates.sql
+++ SQL/triggers/check_offering_dates.sql
@@ -3,32 +3,34 @@ RETURNS TRIGGER AS $$
 DECLARE
     m_start_date DATE;
     m_end_date DATE;
+    c_course_id INTEGER;
+    c_session_id INTEGER;
+    c_offering_launch_date DATE;
+    c_sessions INTEGER;
 BEGIN
-    SELECT MAX(session_date), MIN(session_date) INTO m_end_date, m_start_date
+
+    SELECT COALESCE(NEW.course_id, OLD.course_id), COALESCE(NEW.session_id, OLD.session_id), COALESCE(NEW.offering_launch_date, OLD.offering_launch_date)
+    INTO c_course_id, c_session_id, c_offering_launch_date;
+
+    SELECT MAX(s.session_date), MIN(s.session_date) INTO m_end_date, m_start_date
     FROM Sessions s
-    WHERE NEW.session_id = s.session_id
-        AND NEW.course_id = s.course_id
-        AND NEW.offering_launch_date = s.offering_launch_date;
+    WHERE c_course_id = s.course_id
+    AND c_offering_launch_date = s.offering_launch_date;
 
     IF m_end_date IS NULL OR m_start_date IS NULL THEN
-        RAISE EXCEPTION 'There is no sessions left in the table';
+        RAISE EXCEPTION 'There is 0 sessions left in the table after the operation';
     END IF;
 
     UPDATE CourseOfferings c
     SET offering_start_date = m_start_date,
         offering_end_date = m_end_date
-    WHERE c.course_id = OLD.course_id
-        AND c.offering_launch_date = OLD.offering_launch_date;
+    WHERE c.course_id = c_course_id
+        AND c.offering_launch_date = c_offering_launch_date;
 
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER update_update_date_trigger
-AFTER UPDATE ON Sessions
-FOR EACH ROW WHEN (NEW.session_date IS DISTINCT FROM OLD.session_date)
-EXECUTE FUNCTION check_offering_dates();
-
-CREATE TRIGGER update_others_date_trigger
-AFTER INSERT OR DELETE ON Sessions
+CREATE TRIGGER update_date_trigger
+AFTER INSERT OR DELETE OR UPDATE ON Sessions
 FOR EACH ROW EXECUTE FUNCTION check_offering_dates();
