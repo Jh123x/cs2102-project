8739c548d6d7ba461f38fa8af39b6fd4a5ae1735
Wen Junhua <wenjh1998@gmail.com>
2021-04-08 08:45:17 +0800

    Create session_date_check.sql
    Add a check to enforce that sessions are between offering start date and offering end date

---
+++ SQL/triggers/session_date_check.sql
@@ -0,0 +1,28 @@
+/*Check that the sessions inserted are not before the offering start date or after the offering end date*/
+
+CREATE OR REPLACE FUNCTION session_date_check() RETURNS TRIGGER
+AS $$
+DECLARE
+    start_date DATE;
+    end_date DATE;
+BEGIN
+    SELECT c.offering_start_date, c.offering_end_date INTO start_date, end_date
+    FROM CourseOfferings c
+    WHERE c.course_id = NEW.course_id
+    AND c.offering_launch_date = NEW.offering_launch_date;
+
+    IF NEW.session_date BETWEEN start_date AND end_date THEN
+        RETURN NEW;
+    ELSE
+        RAISE EXCEPTION 'Session cannot be outside offering start date and end date';
+    END IF;
+
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+DROP TRIGGER IF EXISTS session_date_check_trigger ON Sessions;
+CREATE TRIGGER session_date_check_trigger
+AFTER INSERT OR UPDATE ON Sessions
+FOR EACH ROW EXECUTE FUNCTION session_date_check();
