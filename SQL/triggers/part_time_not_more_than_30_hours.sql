aa3b470dba8c1d5bc6e178a646f0b89fef649969
Wen Junhua <wenjh1998@gmail.com>
2021-04-10 14:30:52 +0800

    Update part_time_not_more_than_30_hours.sql

--- SQL/triggers/part_time_not_more_than_30_hours.sql
+++ SQL/triggers/part_time_not_more_than_30_hours.sql
@@ -6,8 +6,6 @@ BEGIN
     SELECT COALESCE(SUM(session_end_hour - session_start_hour), 0) INTO hours
     FROM Sessions
     WHERE instructor_id = NEW.instructor_id AND DATE_TRUNC('month', session_date) = DATE_TRUNC('month', NEW.session_date);
-    /* btw what's with the month check ^? */
-    /* because limited to 30 hours in the same month? instead of entire career? */
 
     IF (hours > 30) THEN
         RAISE EXCEPTION 'Part time Employee working too much';
